3dfe5931e306f181d5555390ce2d89738d22ccbe
Ubuntu <ehtesham@ehtesham.fnbn02fzknbetdfqalhttbqi2h.cx.internal.cloudapp.net>
2021-01-13 08:06:37 +0000

    first

--- main.py
+++ main.py
@@ -21,24 +21,26 @@ app.config['UPLOAD_FOLDER'] =  UPLOAD_FOLDER
 def index():
     return render_template('index.html')
 
-@app.route('/search')
-def search():
-    return render_template('search.html')
-
 @app.route("/record", methods=['POST'])
 def record():
     mycursor = db.cursor()
-    carid = request.form['carid']
-    print('order id :'+carid)
-    mycursor.execute("select * from cars where carid="+carid)
-    Carrows = []
+    transaction_id = request.form['transaction_id']
+    print('trasnsacion id :'+transaction_id)
+    mycursor.execute("select * from orders where transaction_id="+transaction_id)
+    rows = []
     for x in mycursor:
-        Carrows.append(x)
+        rows.append(x)
         print(x)
         print()
+    
+   	
     mycursor.close()
-    return render_template("results.html", len = len(Carrows),Carrows = Carrows)
+    return render_template("search.html", len = len(rows),rows = rows)
 
+
+@app.route('/search',methods=['POST'])
+def search():
+	return render_template('search.html',len=0)
 
 
 # Get the uploaded files
@@ -50,31 +52,42 @@ def uploadFiles():
            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
           # set the file path
            uploaded_file.save(file_path)
-
+	   print('File uploaded successfullu')
            parseCSV(file_path)
           # save the file
       return redirect(url_for('index'))
 
+
+
 def parseCSV(filepath):
-    col_names = ['location', 'price', 'brand', 'model', 'variant', 'year', 'fuel', 'transmission', 'kms', 'owners']
+    col_names = ['Order Date', 'Dispute Register Date ', 'Order ID', 'Transaction',
+       'Ticket Number ', 'Return Type', 'Product Name', 'Category L1',
+       'Amount', 'Seller name', 'Dispute Reason', 'Additional Comments',
+       'Case Status', 'Case closed date', 'Payment Status']
     csvData = pd.read_csv(filepath, names=col_names, header=None)
     mycursor = db.cursor()
+    mycursor.execute("delete from orders")
     for i, row in csvData.iterrows():
         try:
 
-            mySql_insert_query = """INSERT INTO cars (location,brand,owners) VALUES (%s, %s, %s) """
-            if row['location'] and row['brand'] and row['owners']:
-                recordTuple = (row['location'],row['brand'],row['owners'])
-                mycursor.execute(mySql_insert_query, recordTuple)
+            mySql_insert_query = """INSERT INTO orders (order_date,dispute_rg,order_id,transaction_id,ticket_number,return_type,product_name,category,amount,seller_name,dispute_reason,comments,
+       case_status, close_date, payment_status) VALUES (%s, %s, %s, %s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s,%s) """
+	    
+	    	    
 
-            print(i, row['location'],row['brand'],row['owners'])
-
+            
+            recordTuple = (row['Order Date'],row['Dispute Register Date '],row['Order ID'],row['Transaction'],row['Ticket Number '],row['Return Type'],row['Product Name'],row['Category L1'],row['Amount'],row['Seller name'],row['Dispute Reason'],row['Additional Comments'],row['Case Status'],row['Case closed date'],row['Payment Status'])
+            
+	    mycursor.execute(mySql_insert_query, recordTuple)
+	    
+            print(i, recordTuple )
+	    print()
 
         except mysql.connector.Error as error:
             print("Failed to insert into MySQL table {}".format(error))
-    mycursor.close()
+    
     db.commit()
-
+    mycursor.close()
 
 if (__name__ == "__main__"):
      app.run(port = 5000)
